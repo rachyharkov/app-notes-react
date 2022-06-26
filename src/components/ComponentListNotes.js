189531fb941452e0472b8f96f6a53568d908c4c4
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-06-26 16:51:45 +0700

    indikator kosong jika catatan kosong

--- src/components/ComponentListNotes.js
+++ src/components/ComponentListNotes.js
@@ -2,16 +2,24 @@ import React from "react";
 import NoteItem from "./ComponentNotesItem";
 
 function ListElement({notes, onDelete}) {
-    return(
-        <div className="notes-list">
-            {
-                notes.map((notenya) => (
-                    <NoteItem key={notenya.id} onDelete={onDelete} {...notenya} />
-                ))
-            }
-        </div>
 
-    )
+    if(notes.length > 0) {
+        return(
+            <div className="notes-list">
+                {
+                    notes.map((notenya) => (
+                        <NoteItem key={notenya.id} onDelete={onDelete} {...notenya} />
+                    ))
+                }
+            </div>
+        )
+    } else {
+        return(
+            <div className="notes-list">
+                <p className="notes-list__empty-message">Tidak ada catatan</p>
+            </div>
+        )
+    }
 }
 
 export default ListElement
