71603c09ecf202a78c0c6d110c97f34fbc302ce0
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-06-26 12:40:07 +0700

    input validation functionality and visual enhance

--- src/components/ComponentInput.js
+++ src/components/ComponentInput.js
@@ -17,13 +17,39 @@ class InputElement extends React.Component {
         this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this)
     }
 
-    onTextChangeEventHandler(e) {
+    lengthValueDetection(currentLengthValue, maxValue) {
+        document.querySelector('.note-input__title__char-limit').classList.remove('alert-danger')
+        const sisaKarakter = (maxValue - currentLengthValue)
+        if(sisaKarakter <= 0) {
+            document.querySelector('.note-input__title__char-limit').classList.add('alert-danger')
+        }
+        if(sisaKarakter < 0) {
+            return true
+        } else {
+            document.querySelector('.note-input__title__char-limit span').innerHTML = sisaKarakter
+            return false
+        }
+    }
+
+    typeLimitReached(e) {
+        // alert('Judul Kepanjangan')
+        e.target.value = this.state[e.target.name]
+    }
+
+    doChangeText(e, thisValue) {
         this.setState((prevState) => {
             return {
                 ...prevState,
-                [e.target.name]: e.target.value
+                [e.target.name]: thisValue
             }
         })
+    }
+
+    onTextChangeEventHandler(e) {
+        const thisValue = e.target.value
+        const lengthValue = thisValue.length
+        const detection = this.lengthValueDetection(lengthValue, 10)
+        detection ? this.typeLimitReached(e) : this.doChangeText(e, thisValue)
     }
 
     onSubmitEventHandler(e) {
@@ -35,7 +61,7 @@ class InputElement extends React.Component {
         return(
             <form className="note-input" onSubmit={this.onSubmitEventHandler}>
                 <h2 className="note-input__title">Buat Catatan</h2>
-                <p className="note-input__title__char-limit">Sisa Karakter: 0</p>
+                <p className="note-input__title__char-limit">Sisa Karakter: <span>10</span></p>
                 <input placeholder="Judul Catatan" onChange={this.onTextChangeEventHandler} type="text" name="title"/>
                 <textarea placeholder="Input catatan" onChange={this.onTextChangeEventHandler} name="body"></textarea>
                 <button type="submit">Tambah</button>
