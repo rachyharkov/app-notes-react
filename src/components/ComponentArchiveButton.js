77d7152b80972211096a7f8cfbd0e036475ee479
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-06-26 19:53:48 +0700

    integrate with localized date

--- src/components/ComponentArchiveButton.js
+++ src/components/ComponentArchiveButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function ArchiveButton({id, onArchived, archivedStatus}) {
-    return(<button className="note-item__archive-button" onClick={() => onArchived(id)}>{archivedStatus ? 'Aktifkan' : 'Arsipkan'}</button>)
+    return(<button className="note-item__archive-button" onClick={() => onArchived(id)}>{archivedStatus ? 'Pindahkan' : 'Arsipkan'}</button>)
 }
 
 export default ArchiveButton
